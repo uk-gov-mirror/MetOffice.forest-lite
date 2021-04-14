1a11be7777236334abc89bc491556d437d240b4c
andrewgryan <andrew.ryan@metoffice.gov.uk>
2021-04-14 13:07:53 +0100

    begin to build resolvers for colorbrewer colors

--- forest_lite/server/routers/graphql.py
+++ forest_lite/server/routers/graphql.py
@@ -2,17 +2,35 @@
 from fastapi import APIRouter
 from starlette.graphql import GraphQLApp
 import graphene
+from graphene import Field, List, String, Int
+
+
+class Kind(graphene.Enum):
+    SEQUENTIAL = 0
+    DIVERGING = 1
+    CATEGORICAL = 2
+
+
+class Palette(graphene.ObjectType):
+    name = String()
+    kind = Kind()
+    levels = List(Int)
 
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(name=graphene.String(default_value="World!"))
-    colorbrewer = graphene.String()
+    colorbrewer = Field(List(Palette), kind=Kind())
 
-    def resolve_hello(root, info, name):
-        return "Hello, " + name + "!"
-
-    def resolve_colorbrewer(root, info):
-        return "Colorbrewer"
+    def resolve_colorbrewer(root, info, kind=None):
+        # TODO: Parse colorbrewer.json into a list of Palette types
+        palettes = [
+            Palette(name="Reds", kind=Kind.SEQUENTIAL, levels=[3, 5, 6]),
+            Palette(name="Set3", kind=Kind.CATEGORICAL, levels=[3, 4, 5, 6, 7, 8])
+        ]
+        if kind is None:
+            return palettes
+        else:
+            return [palette for palette in palettes
+                    if palette.kind == kind]
 
 
 router = APIRouter()
--- forest_lite/test/test_routers_graphql.py
+++ forest_lite/test/test_routers_graphql.py
@@ -1,14 +1,25 @@
 """
 Test GraphQL endpoint
 """
+from collections import OrderedDict
 from graphene import Schema
+from graphene.test import Client
 from forest_lite.server.routers.graphql import Query
 
 
 
-def test_schema():
+def test_schema_colorbrewer():
     schema = Schema(query=Query)
-    response = schema.execute('''
-        { hello(name: "Bob") }
+    client = Client(schema)
+    response = client.execute('''
+        {
+            colorbrewer(kind: SEQUENTIAL) {
+                name
+            }
+        }
     ''')
-    assert response.data == {"hello": "Hello, Bob!"}
+    assert response["data"]["colorbrewer"] == [
+            OrderedDict([
+                ("name", "Reds")
+            ])
+        ]
